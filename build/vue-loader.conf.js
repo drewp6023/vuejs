83e3ce7d586966df896ae9f6c85275d73b9983be
Drew Pickering <>
2018-01-24 14:36:33 -0700

    Added normalize.css and scss loaders

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -9,7 +9,9 @@ const sourceMapEnabled = isProduction
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
-    extract: isProduction
+    extract: isProduction,
+    'scss': 'vue-style-loader!css-loader!sass-loader',
+    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
@@ -19,4 +21,4 @@ module.exports = {
     img: 'src',
     image: 'xlink:href'
   }
-}
+};
