f097bd9479d6a9ef5c99c53d5a455197c23c6f03
Drew Pickering <>
2018-01-24 12:46:41 -0700

    Added a Home Page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="home-page">
+        <h2>{{ msg }}</h2>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage',
+        data () {
+            return {
+                msg: 'This is the Home Page'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
